a03cbd7dfe29e2b8fed08aa38b4d1e7bca5e4dfa
RODRIGO ARAUJO SANTOS <araujorod@yahoo.com.br>
2026-06-15 23:27:35 -0300

    Primeira auda python

---
+++ 03-Fundamentos_da_Linguagem/variaveis.py
@@ -0,0 +1,11 @@
+nome = "Rodrigo"  # str (String) - Texto
+idade = 52  # int (Inteiro) - Número sem casas decimais
+estado = "Minas Gerais"  # str (String)
+tem_cachorro = True  # bool (Booleano) - Verdadeiro ou Falso
+altura = 1.58  # float (Ponto Flutuante) - Número com casas decimais
+
+print(type(nome))
+print(type(idade))
+print(type(estado))
+print(type(tem_cachorro))
+print(type(altura))
