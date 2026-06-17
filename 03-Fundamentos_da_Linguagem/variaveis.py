086fd41d57bc0b1885318abe84e40c17fd2e18b8
RODRIGO ARAUJO SANTOS <araujorod@yahoo.com.br>
2026-06-17 00:14:30 -0300

    Teste Git Branchas

--- 03-Fundamentos_da_Linguagem/variaveis.py
+++ 03-Fundamentos_da_Linguagem/variaveis.py
@@ -9,3 +9,11 @@ print(type(idade))
 print(type(estado))
 print(type(tem_cachorro))
 print(type(altura))
+
+# Escrever as regras de utilização das variáveis
+# 1. Não podem começar com números
+# 2. Não podem ter espaços
+# 3. Não podem ter caracteres especiais
+# 4. Devem ser escritas em snake_case
+
+print(f"Olá {nome}, você tem {idade} anos e mora em {estado}.")
