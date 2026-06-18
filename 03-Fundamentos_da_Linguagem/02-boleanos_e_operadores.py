9dbc3e5dbd2bce5361cc938f7a30ce542f289c03
araujorod <araujorod@yahoo.com.br>
2026-06-17 21:05:35 -0300

    Aula Operadores e Boleadnos

---
+++ 03-Fundamentos_da_Linguagem/02-boleanos_e_operadores.py
@@ -0,0 +1,14 @@
+# Boleanos
+# True = 0
+# False = 1
+
+# Operadores
+# and = todas as condições devem ser verdadeiras
+# or = pelo menos uma condição deve ser verdadeira
+# not = inverte o valor da condição
+# == = comparação de igualdade
+# != = comparação de desigualdade
+# < = menor que
+# > = maior que
+# <= = menor ou igual a
+# >= = maior ou igual a
