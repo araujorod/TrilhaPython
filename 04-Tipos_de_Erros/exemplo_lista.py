bd4c7d76890ece1e86d2e6a277d77bf7220900a5
RODRIGO ARAUJO SANTOS <araujorod@yahoo.com.br>
2026-06-25 19:54:11 -0300

    Novo arquivo

---
+++ 04-Tipos_de_Erros/exemplo_lista.py
@@ -0,0 +1,15 @@
+# LISTAS
+
+produto: str = "sapato"
+produto_2: str = "camiseta"
+produto_3: str = "videogame"
+
+# clear
+produtos: list = []
+
+produtos.append(produto)
+produtos.append(produto_2)
+produtos.append(produto_3)
+produtos.remove()
+
+print(produtos)
