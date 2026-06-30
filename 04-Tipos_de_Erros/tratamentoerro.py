3a766ccd0036d20369b5ede7b316b86428c5a4db
RODRIGO ARAUJO SANTOS <araujorod@yahoo.com.br>
2026-06-30 13:51:47 -0300

    Aula

--- 04-Tipos_de_Erros/tratamentoerro.py
+++ 04-Tipos_de_Erros/tratamentoerro.py
@@ -10,11 +10,22 @@ for jogo in range(1, 4):
 
     while numero_valido == False:
         try:
-            gols: int = int(input("Quandos gols o Brasil fez?"))
+            gols: int = int(input(f"Quandos gols o Brasil fez no jogo {jogo}?"))
             numero_valido = True
         except ValueError:
             print("Valor inválido. Digite um numero inteiro.")
 
     total_gols += gols
 
-print(f"O Brasil fez {gols} gols.")
+    if gols == 0:
+        print("O Brasil não marcou gols nesse joeo. \n")
+    elif gols == 1:
+        print("O Brasil marcou 1 gols nesse joeo. \n")
+    else:
+        print(f"O Brasil marcou {gols} nesse joeo. \n")
+
+print(f"O Brasil fez {total_gols} gols.")
+
+media: float = total_gols / 3
+
+print(f"Média de gols por jogo: {media:.2f}")
